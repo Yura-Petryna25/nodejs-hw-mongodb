53e294df4ad71bd5674cd6ff45a489ed346ded19
Yura Petryna <yur4ik911@gmail.com>
2025-05-02 15:31:36 +0300

    start hw-07

--- src/constans/index.js
+++ src/constans/index.js
@@ -1,5 +1,5 @@
 import path from 'node:path';
-
+import { fileURLToPath } from 'url';
 export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
@@ -24,3 +24,6 @@ export const CLOUDINARY = {
   API_KEY: 'API_KEY',
   API_SECRET: 'API_SECRET',
 };
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+export const SWAGGER_PATH = path.resolve('docs/swagger.json');
